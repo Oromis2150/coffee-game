8115b9bf9ef4a5435d35116872f077bdc54f43d5
dmreyes3 <dmreyes3@ncsu.edu>
2026-09-25 15:28:14 -0400

    Added + improved animation calls from inputs

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,6 +2,10 @@ extends CharacterBody2D
 
 @onready var animation_sprite = $PlayerSprite
 @export var speed = 50
+var last_direction = "side"
+var is_attacking = false
+var new_direction = Vector2(0,1)
+var animation
 
 func _physics_process(delta):
 	# Get player in
@@ -13,11 +17,27 @@ func _physics_process(delta):
 	if abs(direction.x) == 1 and abs(direction.y) == 1:
 		direction = direction.normalized()
 		
+	if Input.is_action_pressed("ui_sprint"):
+		speed = 75
+	elif Input.is_action_just_released("ui_sprint"):
+		speed = 50
+		
 	# Apply movement
 	var movement = speed * direction * delta
 	
-	move_and_collide(movement)
-	player_animations(direction)
+	if is_attacking == false:
+		move_and_collide(movement)
+		player_animations(direction)
+	if !Input.is_anything_pressed():
+		if is_attacking == false:
+			animation = "idle_" + get_direction(new_direction)
+	
+
+func _input(event):
+	if event.is_action_pressed("ui_attack"):
+		is_attacking = true
+		var animation = "attack_" + get_direction(new_direction)
+		animation_sprite.play(animation)
 
 func get_direction(direction : Vector2):
 	# Normalize direction vector
@@ -25,9 +45,9 @@ func get_direction(direction : Vector2):
 	var default_return = "side"
 	
 	if normalized_direction.y > 0:
-		return "down"
+		return "front"
 	elif normalized_direction.y < 0:
-		return "up"
+		return "back"
 	elif normalized_direction.x > 0:
 		#(right)
 		$PlayerSprite.flip_h = false
@@ -36,15 +56,19 @@ func get_direction(direction : Vector2):
 		#(left)
 		$PlayerSprite.flip_h = true
 		return "side"
-	
 	return default_return
 
 func player_animations(direction : Vector2):
 	var animation
 	# Vector2.ZERO = Vector2(0, 0)
 	if direction != Vector2.ZERO:
-		animation = "walk_" + get_direction(direction)
+		new_direction = direction
+		animation = "walk_" + get_direction(new_direction)
 		animation_sprite.play(animation)
 	else:
-		animation = "idle_" + get_direction(direction)
+		animation = "idle_" + get_direction(new_direction)
 		animation_sprite.play(animation)
+
+
+func _on_player_sprite_animation_finished():
+	is_attacking = false
